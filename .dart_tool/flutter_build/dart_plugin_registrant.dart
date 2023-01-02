5a9f1db14d0e192115b78061fa0a5d849f3a1c89
Codenfast <codenfast@gmail.com>
2023-01-02 14:07:36 +0300

    -mid backup

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -14,7 +14,7 @@ import 'package:image_picker_ios/image_picker_ios.dart';
 import 'package:path_provider_ios/path_provider_ios.dart';
 import 'package:shared_preferences_ios/shared_preferences_ios.dart';
 import 'package:url_launcher_ios/url_launcher_ios.dart';
-import 'package:connectivity_plus_linux/connectivity_plus_linux.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
 import 'package:shared_preferences_linux/shared_preferences_linux.dart';
 import 'package:url_launcher_linux/url_launcher_linux.dart';
@@ -114,10 +114,10 @@ class _PluginRegistrant {
 
     } else if (Platform.isLinux) {
       try {
-        ConnectivityLinux.registerWith();
+        ConnectivityPlusLinuxPlugin.registerWith();
       } catch (err) {
         print(
-          '`connectivity_plus_linux` threw an error: $err. '
+          '`connectivity_plus` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
         rethrow;
